bd585af91af7a0745c661b75244634a41632be7b
Nelson Kamga <kamganelson@gmail.com>
2019-08-20 21:04:28 +0100

    Add `shop_id` to variables

--- services/product.py
+++ services/product.py
@@ -28,6 +28,7 @@ def create():
             "name": data.get("name"),
             "price": data.get("price"),
             "description": data.get("description"),
+            "shop_id": data.get("shop_id")
         },
     )
 
