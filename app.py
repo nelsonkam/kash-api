aea3c8c59c8635de81133cc69ed5ab66ce3f2e15
Nelson Kamga <kamganelson@gmail.com>
2019-09-11 12:19:26 +0100

    remove cors

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import logging
 from flask import Flask
 from flask.logging import default_handler
 from flask_jwt_extended import JWTManager
-from flask_cors import CORS
 import config
 from services import auth, upload, notify, product
 
@@ -12,7 +11,6 @@ from services import auth, upload, notify, product
 # logger.addHandler(default_handler)
 
 app = Flask(__name__)
-CORS(app)
 app.config.from_pyfile("config.py")
 jwt = JWTManager(app)
 app.register_blueprint(auth.blueprint)
