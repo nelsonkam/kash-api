bca0674a61df2a8f23751182ad0b680e7b0ef0c4
Nelson Kamga <kamganelson@gmail.com>
2019-09-12 18:53:12 +0100

    Make JWT optional for uploads

--- services/upload.py
+++ services/upload.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, current_app as app, jsonify, request, abort
-from flask_jwt_extended import get_jwt_identity, jwt_required
+from flask_jwt_extended import get_jwt_identity, jwt_required, jwt_optional
 from utils.upload import save_file, allowed_file
 
 blueprint = Blueprint("upload", __name__, url_prefix="/upload")
 
 
 @blueprint.route("", methods=["POST"])
-@jwt_required
+@jwt_optional
 def upload():
     image = request.files.get("image")
     if image and allowed_file(image.filename):
