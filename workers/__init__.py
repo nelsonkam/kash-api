8b990bd3bacce1a98ba3a819bd6dbbf36c442da3
Nelson Kamga <kamganelson@gmail.com>
2019-09-16 09:25:27 +0100

    change interval to debug worker

--- workers/__init__.py
+++ workers/__init__.py
@@ -5,7 +5,7 @@ from utils import bot
 huey = RedisHuey(url=config.REDIS_URL)
 
 
-@huey.periodic_task(crontab(hour="08", minute="00"))
+@huey.periodic_task(crontab(minute="*/1"))
 def post_metrics():
     return bot.post_metrics()
 
