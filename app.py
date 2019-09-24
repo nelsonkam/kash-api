a459c10c40c53646001637cfe49cfada6dd65c00
Nelson Kamga <kamganelson@gmail.com>
2019-09-24 20:06:45 +0100

    Add category endpoint

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask.logging import default_handler
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 import config
-from services import auth, upload, notify, product, feed
+from services import auth, upload, notify, product, feed, category
 
 # db logger
 # logger = logging.getLogger('orator.connection.queries')
@@ -20,6 +20,7 @@ app.register_blueprint(upload.blueprint)
 app.register_blueprint(notify.blueprint)
 app.register_blueprint(product.blueprint)
 app.register_blueprint(feed.blueprint)
+app.register_blueprint(category.blueprint)
 
 
 if __name__ == "__main__":
---
+++ services/category.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, current_app as app, jsonify, request, abort
+from flask_jwt_extended import get_jwt_identity, jwt_required, jwt_optional
+from utils.graphql import graphql
+
+blueprint = Blueprint("category", __name__, url_prefix="/category")
+
+
+@blueprint.route("/products/<slug>")
+def products(slug):
+    query = """
+    query ($slug: String!){
+        category(where: {slug: {_eq: $slug}}) {
+            name
+            products {
+                id
+                name
+                price
+                product_images {
+                id
+                url
+                }
+            }
+        }
+    }
+    """
+    resp = graphql(query, {"slug": slug})
+
+    if "errors" in resp:
+        code = "GRAPHQL_ERROR"
+        return jsonify({"code": code, "message": error.get("message")}), 400
+
+    return jsonify(resp.get("data"))
