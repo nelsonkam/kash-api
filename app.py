8df55012242f3874b85581ea067f3648defa2a34
Nelson Kamga <kamganelson@gmail.com>
2020-09-03 09:37:42 +0100

    chore: configure logging

--- app.py
+++ app.py
@@ -44,7 +44,10 @@ def register_blueprints(app):
     app.register_blueprint(checkout.blueprint)
     app.register_blueprint(blueprint)
 
-
+def configure_gunicorn_logger(app):
+    gunicorn_error_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers.extend(gunicorn_error_logger.handlers)
+    app.logger.setLevel(logging.DEBUG)
 
 def create_app(config_file="config.py"):
     app = Flask(__name__)
@@ -53,5 +56,6 @@ def create_app(config_file="config.py"):
     jwt.init_app(app)
     db.init_app(app)
     register_blueprints(app)
+    configure_gunicorn_logger(app)
     return app
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 python db.py migrate -f -n
-exec gunicorn --bind=0.0.0.0:5000 "app:create_app()" -w 2
+exec gunicorn --bind=0.0.0.0:5000 "app:create_app()" -w 2 --log-level DEBUG
