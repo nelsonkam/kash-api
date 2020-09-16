9b1a4a8c807bd5ee347819c12750f50b40dbba7c
Nelson Kamga <kamganelson@gmail.com>
2020-09-16 11:54:35 +0100

    Change expiration time

--- kweek_api/settings.py
+++ kweek_api/settings.py
@@ -151,7 +151,7 @@ PHONE_VERIFICATION = {
     "TOKEN_LENGTH": 6,
     "MESSAGE": _("Your verification code for {app} is: {security_code}"),
     "APP_NAME": "Kweek",
-    "SECURITY_CODE_EXPIRATION_TIME": 3600 * 0.5,
+    "SECURITY_CODE_EXPIRATION_TIME": 3600 * 6,
     "VERIFY_SECURITY_CODE_ONLY_ONCE": False,
 }
 
