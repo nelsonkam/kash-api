3ca4d385b7ae08b3bb4cf04c8f8e6f436440b622
nelson <kamganelson@gmail.com>
2021-02-22 18:11:26 +0100

    fix: stripe checkout prices

--- core/utils/payment.py
+++ core/utils/payment.py
@@ -2,7 +2,10 @@ import requests
 import stripe
 from django.conf import settings
 from kkiapay import Kkiapay
-FEDAPAY_URL = "https://sandbox-api.fedapay.com" if settings.DEBUG else "https://api.fedapay.com"
+
+FEDAPAY_URL = (
+    "https://sandbox-api.fedapay.com" if settings.DEBUG else "https://api.fedapay.com"
+)
 
 
 def fedapay_request(method, url, data=None):
@@ -13,11 +16,9 @@ def fedapay_request(method, url, data=None):
 class Payment:
     @classmethod
     def get_processor_cls(cls, method):
-        return {
-            "card": StripePayment,
-            "cash": CashOnDelivery,
-            "momo": KKiaPayment
-        }[method]
+        return {"card": StripePayment, "cash": CashOnDelivery, "momo": KKiaPayment}[
+            method
+        ]
 
     @classmethod
     def create_transaction(cls, checkout, method="card", **kwargs):
@@ -27,9 +28,9 @@ class Payment:
             except Exception as e:
                 print("[stripe error]", e)
                 return None
-        elif method == 'momo':
+        elif method == "momo":
             return KKiaPayment.create_transaction(checkout, **kwargs)
-        elif method == 'cash':
+        elif method == "cash":
             return CashOnDelivery.create_transaction(checkout, **kwargs)
         else:
             raise NotImplemented
@@ -42,16 +43,20 @@ class Payment:
 
 
 class KKiaPayment:
-
     @classmethod
     def create_transaction(cls, checkout, **kwargs):
         return {"processor": "kkiapay", "amount": checkout.total()}
 
     @classmethod
     def verify_transaction(cls, transaction_id=None, **kwargs):
-        k = Kkiapay(settings.KKIAPAY_PUBLIC_KEY, settings.KKIAPAY_PRIVATE_KEY, settings.KKIAPAY_SECRET_KEY, sandbox=settings.DEBUG)
+        k = Kkiapay(
+            settings.KKIAPAY_PUBLIC_KEY,
+            settings.KKIAPAY_PRIVATE_KEY,
+            settings.KKIAPAY_SECRET_KEY,
+            sandbox=settings.DEBUG,
+        )
         transaction = k.verify_transaction(transaction_id)
-        return transaction.status == 'SUCCESS'
+        return transaction.status == "SUCCESS"
 
 
 class StripePayment:
@@ -61,21 +66,34 @@ class StripePayment:
             {
                 "price_data": {
                     "currency": item.product.currency_iso,
-                    "product_data": {"name": item.product.name,},
-                    "unit_amount": round(item.product.price),
+                    "product_data": {"name": item.product.name},
+                    "unit_amount": round(
+                        item.product.price
+                        if item.product.currency_iso.lower() == "xof"
+                        else item.product.price * 100
+                    ),
                 },
                 "quantity": item.quantity,
             }
-            for item in checkout.cart.items.prefetch_related("product", "product__images").all()
+            for item in checkout.cart.items.prefetch_related(
+                "product", "product__images"
+            ).all()
         ]
-        items.append({
-            "price_data": {
-                "currency": checkout.shipping_option.get("price").get("currency"),
-                "product_data": {"name": "Shipping Fees", },
-                "unit_amount": round(checkout.shipping_fees()),
-            },
-            "quantity": 1,
-        })
+        shipping_price = checkout.shipping_option.get("price")
+        items.append(
+            {
+                "price_data": {
+                    "currency": shipping_price.get("currency"),
+                    "product_data": {"name": "Shipping Fees"},
+                    "unit_amount": round(
+                        checkout.shipping_fees()
+                        if shipping_price.get("currency").lower() == "xof"
+                        else checkout.shipping_fees() * 100
+                    ),
+                },
+                "quantity": 1,
+            }
+        )
         session = stripe.checkout.Session.create(
             payment_method_types=["card"],
             line_items=items,
@@ -83,10 +101,8 @@ class StripePayment:
             customer_email=checkout.customer.email,
             client_reference_id=checkout.customer.id,
             success_url=f"{kwargs.get('origin')}/order/{checkout.uid}/confirmed/?session_id={{CHECKOUT_SESSION_ID}}",
-            cancel_url=kwargs.get('return_url'),
-            metadata={
-                'checkout_id': checkout.uid,
-            }
+            cancel_url=kwargs.get("return_url"),
+            metadata={"checkout_id": checkout.uid},
         )
 
         return {"processor": "stripe", "session_id": session.id}
@@ -94,7 +110,8 @@ class StripePayment:
     @classmethod
     def verify_transaction(cls, session_id=None, **kwargs):
         session = stripe.checkout.Session.retrieve(session_id)
-        return session.payment_status == 'paid'
+        return session.payment_status == "paid"
+
 
 class CashOnDelivery:
     @classmethod
