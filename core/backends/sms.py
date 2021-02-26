b47f3d6794d30ec6ab24103d5e9e2c8f8957e1ed
nelson <kamganelson@gmail.com>
2021-02-26 22:52:05 +0100

    feat: add test phone number

--- core/backends/sms.py
+++ core/backends/sms.py
@@ -1,9 +1,33 @@
+from abc import ABC
+
 import boto3
 from django.conf import settings
 from phone_verify.backends.base import BaseBackend
+from phone_verify.models import SMSVerification
 
 
-class ConsoleSMSBackend(BaseBackend):
+class BaseSMSBackend(BaseBackend, ABC):
+    def create_security_code_and_session_token(self, number):
+        if number in settings.PHONE_VERIFICATION.get("TEST_PHONE_NUMBERS"):
+            security_code = "123456"
+        else:
+            security_code = self.generate_security_code()
+
+        session_token = self.generate_session_token(number)
+
+        # Delete old security_code(s) for phone_number if already exists
+        SMSVerification.objects.filter(phone_number=number).delete()
+
+        # Default security_code generated of 6 digits
+        SMSVerification.objects.create(
+            phone_number=number,
+            security_code=security_code,
+            session_token=session_token,
+        )
+        return security_code, session_token
+
+
+class ConsoleSMSBackend(BaseSMSBackend):
     def send_sms(self, number, message):
         print(f"SMS to {number}: {message}")
 
@@ -12,7 +36,7 @@ class ConsoleSMSBackend(BaseBackend):
             self.send_sms(number, message)
 
 
-class AmazonSMSBackend(BaseBackend):
+class AmazonSMSBackend(BaseSMSBackend):
 
     def send_sms(self, number, message):
         client = boto3.client(
