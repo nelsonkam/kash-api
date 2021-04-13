439d888f88541312fb3d6a893ff3e17597b5f31e
nelson <kamganelson@gmail.com>
2021-04-13 16:39:03 +0100

    fix: disable throttling for users

--- kweek_api/settings.py
+++ kweek_api/settings.py
@@ -155,11 +155,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle'
     ],
     'DEFAULT_THROTTLE_RATES': {
         'anon': '100/day',
-        'user': '300/min'
     }
 }
 
