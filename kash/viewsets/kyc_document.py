df88bef6713fcb619833aa3270dfc25605180435
nelson <kamganelson@gmail.com>
2021-05-10 01:05:13 +0100

    fix: document upload

--- kash/viewsets/kyc_document.py
+++ kash/viewsets/kyc_document.py
@@ -1,6 +1,7 @@
 from uuid import uuid4
 
 from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
@@ -26,6 +27,8 @@ class KYCDocumentViewSet(ModelViewSet):
         kyc_doc = self.get_object()
         doc_image = request.data.get('document')
         selfie = request.data.get('selfie')
+        if not (doc_image or selfie):
+            raise ValidationError("A document image and selfie is required.")
         kyc_doc.doc_url = upload_content_file(doc_image, f"{uuid4()}-{doc_image.name or 'doc.jpg'}", "private")
         kyc_doc.selfie_url = upload_content_file(selfie, f"{uuid4()}-{selfie.name or 'selfie.jpg'}", "private")
         kyc_doc.save()
