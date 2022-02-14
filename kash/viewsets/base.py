219a2722442abc512c602deb8b87e17f836e354a
Nelson <kamganelson@gmail.com>
2022-02-14 14:02:23 +0000

    update docker-compose.yml

--- kash/viewsets/base.py
+++ kash/viewsets/base.py
@@ -1,7 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
+
+from core.models import User
 from kash.models import UserProfile
 
 class BaseViewSet(ModelViewSet):
     def perform_authentication(self, request):
         super().perform_authentication(request)
-        self.request.profile = UserProfile.objects.get(user=self.request.user)  # qc: 1
+        if isinstance(self.request.user, User):
+            self.request.profile = UserProfile.objects.get(user=self.request.user)  # qc: 1
