ba64a00458f4500df34027e8f47471c1c1e52aee
Nelson Kamga <kamganelson@gmail.com>
2022-02-27 14:54:55 +0000

    Update settings.py

--- kash_api/settings.py
+++ kash_api/settings.py
@@ -170,7 +170,7 @@ REST_FRAMEWORK = {
 SMS_BACKEND = (
     "core.backends.sms.ConsoleSMSBackend"
     if DEBUG
-    else "core.backends.sms.MessageBirdSMSBackend"
+    else "core.backends.sms.AmazonSMSBackend"
 )
 
 PHONE_VERIFICATION = {
