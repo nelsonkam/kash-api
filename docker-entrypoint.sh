a4f751dc260d3d02180d68511247b47f79529d9b
Nelson <kamganelson@gmail.com>
2022-03-01 14:54:15 +0000

    - fixes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 python manage.py migrate --no-input
-python manage.py test --no-input --failfast --buffer
 python manage.py collectstatic --no-input
 mkdir -p static/uploads
 exec gunicorn --bind=0.0.0.0:5000 -w 6 app_main.wsgi:application --timeout 300
