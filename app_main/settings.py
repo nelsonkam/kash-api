7724b98543ec36fd676f0abfe70fad57e72b6b63
Nelson <kamganelson@gmail.com>
2022-03-07 12:47:38 +0000

    chore: sentry config

--- app_main/settings.py
+++ app_main/settings.py
@@ -242,7 +242,7 @@ if not DEBUG:
     sentry_sdk.init(
         dsn=env("SENTRY_KEY"),
         integrations=[DjangoIntegration()],
-        traces_sample_rate=1.0,
+        traces_sample_rate=0.1,
         # If you wish to associate users to errors (assuming you are using
         # django.contrib.auth) you may enable sending PII data.
         send_default_pii=True,
