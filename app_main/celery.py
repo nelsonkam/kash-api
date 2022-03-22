dc43fb2720ced556661103566651b296a748d070
Nelson <kamganelson@gmail.com>
2022-03-22 17:48:02 +0000

    fix: celery

--- app_main/celery.py
+++ app_main/celery.py
@@ -7,4 +7,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app_main.settings")
 
 app = Celery("app_main")
 app.config_from_object("django.conf:settings", namespace="CELERY")
+
+app.conf.redis_backend_use_ssl = {"ssl_cert_reqs": "none"}
 app.autodiscover_tasks()
